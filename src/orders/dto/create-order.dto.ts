4213ac02267703bc7aa769f3c395330dd4a8530d
sbsmrth <andresf.mrh@gmail.com>
2025-07-27 10:26:49 -0500

    refactor(orders): update DTO imports and exports for order items
    - Refactored imports in create-order.dto.ts to use array and nested validation decorators
    
    - Exported order-item.dto from dto/index.ts for improved modularity

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,29 +1,19 @@
-import { OrderStatus } from 'generated/prisma';
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
-import { OrderStatusList } from '../enum/order.enum';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
-
-  @IsNumber()
-  @IsPositive()
-  totalItems: number;
-
+  /*
   @IsEnum(OrderStatusList, {
     message: `Possible status values are ${JSON.stringify(OrderStatusList)}`,
   })
   @IsOptional()
   status: OrderStatus = OrderStatus.PENDING;
+  */
 
-  @IsBoolean()
-  @IsOptional()
-  paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 }
